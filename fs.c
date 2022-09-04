9e13d2cfbc7fced8d827be744db4c85604ef34e3
LeozeraVal <leovmorales@hotmail.com>
2022-09-04 18:57:29 -0300

    comentarios mais caquinha corrigida

--- fs.c
+++ fs.c
@@ -42,6 +42,7 @@ typedef struct {
 
 dir_entry dir[DIRENTRIES];
 
+// Funcao Auxiliar interna do fs que retorna o proximo setor livre da fat.
 unsigned short __fs_next_free_fat() {
   for (size_t i = 33; i < bl_size(); i++) {
     if (fat[i] == 1) return i;
@@ -49,6 +50,15 @@ unsigned short __fs_next_free_fat() {
   return -1;
 }
 
+// Funcao Auxiliar interna do fs que escreve a fat e o unico dir no arquivo.
+void __fs_write_fat_dir_disk() {
+  for (size_t i = 0; i < 32; i++) {
+    bl_write(i, ((char*) &fat) + i*CLUSTERSIZE);
+  }
+  bl_write(32,(char*) &dir);
+}
+
+// Funcao Auxiliar interna do fs que printa a fat guardada em memoria.
 void print_fat(){
   buffer = (char *) fat;
   for (size_t i = 0; i < sizeof(fat); i++) {
@@ -60,19 +70,24 @@ void print_fat(){
 }
 
 int fs_init() {
-  //Buffer aponta para fat;
+  //Buffer aponta para fat.
   buffer = (char *) fat;
   for (size_t i = 0; i < 32; i++) {
+    // Cada chamada de bl_read le um setor inteiro, portanto precisamos somar esse valor ao multiplicador de indice.
     bl_read(i, buffer+(i*SECTORSIZE));
   }
 
-  // Checagens de Formtação;
+  // Checagens de Formtação.
   int formatado = 1;
   size_t i;
   for (i = 0; i < 32; i++) {
+    // Itera pelos primeiros 32 setores da fat checando se estao com o valor corredo.
     if (fat[i] != 3) formatado = 0;
   }
+  // Faz o mesmo para o setor 33, que tem que possuir o valor 4 para o unico diretorio do arquivo.
   if (fat[i] != 4) formatado = 0;
+
+  // Finaliza lendo o Setor do diretorio e carregando na memoria.
   bl_read(32,(char*) dir);
 
   if (!formatado) {
@@ -84,35 +99,38 @@ int fs_init() {
 }
 
 int fs_format() {
-  // Em memoria azucrinamos a fat
+  // Primeiro populamos a fat em memoria, primeiramente os 32 primeiros setores com o valor 3.
+  // Depois o setor 33 com o valor 4 de diretorio.
   for (size_t i = 0; i < 32; i++) {
     fat[i] = 3;
   }
   fat[32] = 4;
+
+  // Para o resto da fat ate o bl_size, populamos.
   for (size_t i = 33; i < bl_size(); i++) {
     fat[i] = 1;
   }
   
-  // Entao escrevemos a fat no disco :)
+  // Entao escrevemos a fat no disco.
   for (size_t i = 0; i < 32; i++) {
     bl_write(i, ((char*) &fat) + i*CLUSTERSIZE);
   }
 
-  // Em memória azucrinamos o dir
+  // Em memória populamos o dir.
   for (size_t i = 0; i < DIRENTRIES; i++) {
     dir[i].used = 0;
     dir[i].name[0] = '\0';
     dir[i].first_block = -1;
     dir[i].size = 0;
   }
-  // Entao escrevemos o dir no disco :)
+
+  // Entao escrevemos o dir no disco.
   bl_write(32, (char*) &dir);
 
   return 1;
 }
 
 int fs_free() {
-  //printf("Função não implementada: fs_free\n");
   int free_blocks = 0;
   for (size_t i = 0; i < bl_size(); i++) {
     if (fat[i] == 1) free_blocks++;
@@ -121,58 +139,85 @@ int fs_free() {
 }
 
 int fs_list(char *buffer, int size) {
-  //checar se esta sendo usado e imprimir?
-  printf("Função não implementada: fs_list\n");
-  return 0;
+  // Criamos uma string temporaria que ira ser escrita no buffer.
+  char temp[38];
+
+  // Por algum motivo precisamos limpar o buffer antes de efetuar o strcat,
+  // Pelo que entendemos deveria vir limpa do shell.c, portanto utilizamos esse metodo
+  // apesar de ser invasivo.
+  buffer[0] = '\0';
+
+  for (size_t i = 0; i < DIRENTRIES; i++) {
+    if (dir[i].used == 1) {
+      // Para cada arquivo utilizado, printamos seu formato na string temp.
+      sprintf(temp, "%s\t\t%d\n", dir[i].name, dir[i].size);
+      // Depois concatenamos no buffer.
+      strcat(buffer,temp);
+    } 
+  }
+
+  return 1;
 }
 
 int fs_create(char* file_name) {
   int alvo = -1;
   for (size_t i = 0; i < DIRENTRIES; i++) {
+    // Procuramos o primeiro local vazio.
     if (dir[i].used == 0 && alvo == -1) alvo = i;
     
-    if(strcmp(dir[i].name, file_name) == 0) {
+    // Caso encontremos um arquivo com o mesmo nome retornamos erro.
+    if(strcmp(dir[i].name, file_name) == 0 && dir[i].used == 1) {
       printf("Arquivo já existe!⚠⚠⚠⚠⚠\n");
       return 0;      
     }
   }
 
+  // Se nao achamos espaco vazio retornamos e printamos erro.
   if (alvo == -1) {
     printf("ACABOU O ESPAÇO!⚠⚠⚠⚠⚠\n");
     return 0;
   }
 
+  // Efetuamos a checagem de tamanho de nome de arquivo.
   int tamanho_nome = strlen(file_name);
   if(tamanho_nome > 24) {
     printf("Nome de arquivo muito grande!⚠⚠⚠⚠⚠\n");
     return 0;
   } 
+
+  // Populamos a estrutura de dir com as informacoes passadas.
   strncpy(dir[alvo].name, file_name, tamanho_nome);
   dir[alvo].name[tamanho_nome] = '\0';
   dir[alvo].size = 0;
   dir[alvo].used = 1;
+  // Utilizamos a funcao auxiliar para buscar o proximo setor vazio na fat.
   unsigned short target_block = __fs_next_free_fat();
-  
   dir[alvo].first_block = target_block;
+
+  // Escrevemos na fat em memoria o setor ocupado.
   fat[target_block] = 2;
   
-  //ESCREVER EM DISCO
+  // Finalmente escrevemos no disco a fat.
+  __fs_write_fat_dir_disk();
   return 1;
 }
 
 int fs_remove(char *file_name) {
   for (size_t i = 0; i < DIRENTRIES; i++) {
-    if( dir[i].used == 1 && strcmp(dir[i].name, file_name) == 0) {
+    // Procuramos o arquivo fornecido na estrutura de diretorio.
+    if(dir[i].used == 1 && strcmp(dir[i].name, file_name) == 0) {
       dir[i].used = 0;
       unsigned short target_block = dir[i].first_block;
       unsigned short new_target;
-      while (target_block != 2) {
+      do {
+        // Utilizamos new_target para iterar pelos blocos do arquivo na fat
+        // e modificamos para apontar setor vazio ate chegarmos no 2, que limpamos e saimos do loop.
         new_target = fat[target_block];
         fat[target_block] = 1;
         target_block = new_target; 
-      }
-      fat[target_block] = 1;
-      //ESCREVER EM DISCO
+      } while (target_block != 2);
+      // Escrevemos a fat e dir no disco.
+      __fs_write_fat_dir_disk();
       return 1;
     }
   }
